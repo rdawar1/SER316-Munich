f0c0de835409dd53068e37f2960f1cc7791b56e6
mdturne2 <mdturne2@asu.edu>
2017-04-21 09:05:58 -0700

    Revert "After hours of searching, I finally found the place where I needed to…"

--- src/net/sf/memoranda/ui/TaskProgressEditor.java
+++ src/net/sf/memoranda/ui/TaskProgressEditor.java
@@ -41,8 +41,7 @@ public class TaskProgressEditor extends JPanel implements TableCellEditor{
 				}else{
 					current.setProgress( current.getProgress()-5 );
 				}
-				//repaint();
-				table.repaint();
+				repaint();
 			}
 		});
 		setLayout(new java.awt.BorderLayout());
@@ -103,3 +102,5 @@ public class TaskProgressEditor extends JPanel implements TableCellEditor{
 	public boolean shouldSelectCell(java.util.EventObject var0){return true;}	
     
 }
+
+
