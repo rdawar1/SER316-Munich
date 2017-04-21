8fd4d5c9f2dcb834d91b29b481655d9b68cc7ff9
aoboaten1 <aoboaten@asu.edu>
2017-04-20 20:48:44 -0700

    Sprint-4-Task#77
    This commit enables the user to input his/her own important dates

--- src/net/sf/memoranda/ui/Impdates.java
+++ src/net/sf/memoranda/ui/Impdates.java
@@ -1,20 +1,30 @@
+
 //The Impdates list all the important dates in your 
 //calender so you do not forget about them.
 //The important dates include dates you have tasks due and holidays
 package net.sf.memoranda.ui;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
-public class Impdates{
+public class Impdates extends JDialog implements ActionListener{
+	
+	JTextField text, text1;
+	 JButton submit, convert1;;
+	public boolean unitTest;
 	
 	public boolean x = false;
 public Impdates() {
+		
+	
  JFrame frame = new JFrame("Important Dates");
  JPanel panel = new JPanel();
  panel.setLayout(new FlowLayout());
  JPanel panel1 = new JPanel();
  panel1.setLayout(new FlowLayout());
- JLabel label = new JLabel("This due This Week!"); 
+ JLabel label = new JLabel("Things due This Week!"); 
 
  JButton button = new JButton();
  button.setText("Open List");
@@ -48,5 +58,37 @@ public Impdates() {
  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  frame.setVisible(true);
  x = true;
+
+
+text=new JTextField("Add Important Date");	
+submit=new JButton("submit");
+this.setModal(true);
+this.setLayout(null);
+this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+this.setLocationRelativeTo(null);
+this.setSize(370, 300);
+this.setTitle("Access Important Dates");
+	 this.setResizable(false);
+
+
+text.setBounds(30,50,280,30);
+
+submit.setBounds(30,150,280,30);
+
+
+
+submit.addActionListener(this);
+this.add(text);
+this.add(submit);
+
+
+
+
+this.setVisible(true); 
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	
 }
 }
